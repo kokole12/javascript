f51658908a757dfacaa333647c3919fe87de030e
kokole12 <kokoleismail886@gmail.com>
2023-06-19 20:30:15 +0300

    printing the rectangle in shapes X

---
+++ 1-javascript/3-rectangle.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+class Rectangle{
+    constructor(w, h){
+        if (w > 0 && h > 0){
+            this.width = w;
+            this.height = h;
+        }
+    }
+
+    print(){
+        for(let i = 0; i < this.height; i++){
+            let myvar = "";
+            let y = 0
+
+            while(y < this.width){
+                myvar = myvar + "X";
+                y++;
+            }
+            console.log(myvar);
+        }
+    }
+}
+
+module.exports = Rectangle
