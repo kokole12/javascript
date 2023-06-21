fe9f95e3bf8851b07c0c98e5933eb57ca6eefa2f
kokole12 <kokoleismail886@gmail.com>
2023-06-21 18:38:18 +0300

    promises

--- 3-javascript/3-promise.js
+++ 3-javascript/3-promise.js
@@ -10,9 +10,24 @@ const promise =  new Promise((resolve, reject) =>{
     }, 3000);
 });
 
+const namePromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(["Annie", "Jane", "Anifa"])
+    }, 1000);
 
-promise.then(response => {
-    console.log(response);
+    setTimeout(() => {
+        reject("no data")
+    }, 1000);
+})
+
+// promise.then(response => {
+//     console.log(response);
+// }).catch(error => {
+//     console.log(error);
+// })
+
+Promise.all([promise, namePromise]).then(data => {
+    const [message, names] = data;
 }).catch(error => {
     console.log(error);
 })
