27a1233dff81cf25843a1d10abb25549780a74de
kokole12 <kokoleismail886@gmail.com>
2023-06-28 23:48:06 +0300

    string literals

---
+++ es6js/6-main.js
@@ -0,0 +1,5 @@
+/* eslint-disable import/extensions */
+// eslint-disable-next-line import/no-unresolved
+import getSanFranciscoDescription from './6-string-interpolations.js';
+
+console.log(getSanFranciscoDescription());
