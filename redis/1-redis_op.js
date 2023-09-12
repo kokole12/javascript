05a6a6d41f1546427928089666d20992ea234e46
kokole12 <kokoleismail886@gmail.com>
2023-09-12 14:51:20 +0300

    setting and getting values"

---
+++ redis/1-redis_op.js
@@ -0,0 +1,28 @@
+import { redis } from "kue";
+import { createClient, print} from "redis";
+
+const client = createClient();
+
+client.on('connect', () => {
+    console.log('Redis client connected to the server')
+});
+
+client.on('error', (error) => console.log(`Redis client not connected to the server: ${error}`));
+
+const setNewSchool = (schoolName, value) => {
+    client.set(schoolName, value, print);
+}
+
+const displaySchoolValue =(schoolName) => {
+    client.get(schoolName, (error, result) => {
+        if (error) {
+            console.log(error);
+            throw error;
+        }
+        console.log(result);
+    });
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
