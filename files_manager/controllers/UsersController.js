dfcf8db7da002b59129b376032d07aa9ee2b9acc
kokole12 <kokoleismail886@gmail.com>
2023-09-17 11:23:14 +0300

    added bull queue

--- files_manager/controllers/UsersController.js
+++ files_manager/controllers/UsersController.js
@@ -1,10 +1,13 @@
 import sha1 from 'sha1';
+import Queue from 'bull';
 import dbClient from '../utils/db';
+
+const userQueue = new Queue('usersQueue');
 
 export default class UsersController {
   static async postNew(req, res) {
-    const email = req.body || req.body.email || null;
-    const password = req.body || req.body.password || null;
+    const { email } = req.body;
+    const { password } = req.body;
 
     if (!email) {
       res.status(400).json({ error: 'Missing email' });
@@ -17,13 +20,16 @@ export default class UsersController {
     }
 
     const users = await dbClient.db.collection('users');
-    const user = users.findOne({ email });
-    if (user) {
-      res.status(400).json({ error: 'Already exists' });
-      return;
-    }
-
-    const newUser = await users.insertOne({ email, password: sha1(password) });
-    res.status(201).json(newUser);
+    users.findOne({ email }, (err, user) => {
+      if (user) {
+        res.status(400).json({ error: 'Already exists' });
+      } else {
+        users.insertOne({ email, password: sha1(password) })
+          .then((result) => {
+            res.status(201).json({ id: result.insertedId, email });
+            userQueue.add({ userId: result.insertedId });
+          });
+      }
+    });
   }
 }
